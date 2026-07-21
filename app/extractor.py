fe13f08bd22d771fcf285b6303818e69f88a7bfe
Harshitha Alapaka <harshithaalapaka@Harshithas-MacBook-Air.local>
2026-07-21 10:20:11 +0530

    second commit

--- app/extractor.py
+++ app/extractor.py
@@ -9,3 +9,19 @@ def extract_text(file_data : bytes, file_name :str)-> str:
         return _extract_docx(file_data)
     else:
         raise ValueError("Unsupported file type. Use PDF or DOCX.")
+
+def _extract_pdf(file_data:bytes) -> str:
+    text=[]
+    with pdfplumber.open(io.BytesIO(filebytes)) as pdf:
+        for page in pdf.pages:
+            page_text=page.extract_text()
+            if page_text:
+                text.append(page_text)
+    return "\n".join(text)
+
+def _extract_docx(file_bytes:bytes)->str:
+    doc= Document(io.BytesIO(file_bytes))
+    text=[]
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+    return "\n".join(text)
