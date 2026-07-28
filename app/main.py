165c0fd1c182cf16f2697875167da8d770792fa5
Harshithaalapaka <harshithaalapaka@gmail.com>
2026-07-28 10:11:35 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -3,3 +3,15 @@ from app.extractor import extract_text
 from app.agent import match_resume_to_job
 
 app = FastAPI(title="Resume Analyser")
+
+@app.get("/")
+def root():
+    return {"status" : "Resume_Analyser API is running"}
+
+@app.post("/analyse")
+async def analyse_resume(
+    file : UploadFile = File(...),
+    Job_description : str = Form(...)
+):
+
+    
